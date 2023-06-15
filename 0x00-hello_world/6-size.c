afd1f78297076f98c0f8c8593616efe862d4b8a9
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-06-15 15:07:32 -0700

    .

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - sizes implementation
+ * Return: 0 if no errors
+ */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(char);
+	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(int);
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(long);
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(long long);
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(float);
+	return (0);
+}
