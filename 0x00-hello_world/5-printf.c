14365d6eb181abe93fdb4b4500743df3e4af9876
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-06-15 14:23:39 -0700

    .

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - printf implementation
+ * Return: 0 if no errors
+ */
+int main(void)
+{
+	printf("%s","with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
