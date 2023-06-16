6d206234005b68ae323e7c934612d943e74457c1
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-06-16 16:21:58 -0700

    .

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,6 +15,5 @@ int main(void)
 	if (n > 0) printf("%d is positive\n", n);
 	if (n == 0) printf("%d is zero\n", n);
 	if (n < 0) printf("%d is negative\n", n);
-	else printf("n is this weird num: %d",n);
 	return (0);
 }
