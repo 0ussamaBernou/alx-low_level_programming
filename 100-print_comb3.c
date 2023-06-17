9c843317b8f805622325b6673bec01495ffaff78
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-17 19:11:37 +0000

    added 100

---
+++ 100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 expected
+ */
+int main(void)
+{
+        int limit = 10;
+
+        for (int i = 0; i < limit; i++)
+        {
+                for (int j = 1; j < limit; j++)
+                {
+                        if (j > i)
+                        {
+                                putchar(48 + i);
+                                putchar(48 + j);
+
+                                if (j == limit - 1 && i == limit - 2)
+                                        putchar('\n');
+                                else
+                                {
+                                        putchar(',');
+                                        putchar(' ');
+                                }
+                        }
+                        else
+                                continue;
+                }
+                
+        }
+        
+        return (0);
+}
