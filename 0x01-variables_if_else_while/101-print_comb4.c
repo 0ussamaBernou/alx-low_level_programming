d7d443e2c866be5e20e774b63a3ea7f5805bf37c
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-17 20:34:47 +0000

    added 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+/**
+ * main - main function
+ *
+ * Return: 0 expected
+ */
+int main(void)
+{
+	int i, j, k, limit;
+
+	limit = 10;
+
+	for (i = 0; i < limit; i++)
+	{
+		for (j = 1; j < limit; j++)
+		{
+			if (j > i)
+			{
+				for (k = 0; k < limit; k++)
+				{
+					if (k > j)
+					{
+						putchar(48 + i);
+						putchar(48 + j);
+						putchar(48 + k);
+
+						if (j == limit - 2 &&
+						    i == limit - 3 &&
+						    k == limit - 1)
+							putchar('\n');
+						else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+			else
+				continue;
+		}
+	}
+
+	return (0);
+}
