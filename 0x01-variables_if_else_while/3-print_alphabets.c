4cbad37ff26a5d72b7cf17869cd1755b1c23f5fb
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-06-17 01:43:25 +0100

    auto pushed

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - main func
+ *
+ * Return: 0
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch++);
+	}
+
+	/* from z to A (ascii) */
+	ch -= (32+26);
+
+	while (ch <= 'Z')
+	{
+		putchar(ch++);
+	}
+	putchar('\n');
+
+	return (0);
+}
