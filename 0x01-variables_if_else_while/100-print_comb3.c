8946d7be88663a22674919d6d2cdd0ee06959bd8
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-17 19:32:03 +0000

    converted spaces to tabs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,30 +7,30 @@
  */
 int main(void)
 {
-        int i, j, limit;
-        limit = 10;
+	int i, j, limit;
+	limit = 10;
 
-        for (i = 0; i < limit; i++)
-        {
-                for (j = 1; j < limit; j++)
-                {
-                        if (j > i)
-                        {
-                                putchar(48 + i);
-                                putchar(48 + j);
+	for (i = 0; i < limit; i++)
+	{
+		for (j = 1; j < limit; j++)
+		{
+			if (j > i)
+			{
+				putchar(48 + i);
+				putchar(48 + j);
 
-                                if (j == limit - 1 && i == limit - 2)
-                                        putchar('\n');
-                                else
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                        else
-                                continue;
-                }
-        }
+				if (j == limit - 1 && i == limit - 2)
+					putchar('\n');
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+				continue;
+		}
+	}
 
-        return (0);
+	return (0);
 }
