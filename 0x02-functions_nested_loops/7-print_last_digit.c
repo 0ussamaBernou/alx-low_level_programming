75a746be987631635ae1877d321cfcd8fb68f987
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-20 13:48:50 +0000

    .

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,4 +10,12 @@
  * Return: last digit of n
  */
 
-int print_last_digit(int n) { return _abs(n % 10); }
+int print_last_digit(int n)
+{
+	int last;
+
+	last = _abs(n % 10);
+
+	_putchar('0' + last);
+	return last;
+}
