5e37d1c8db40fac695458eab3fc1d9621cc96356
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-20 13:35:30 +0000

    .

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_last_digit - return the last digit
+ *
+ * @n: number to process
+ *
+ * Return: absolute value of n
+ */
+
+int print_last_digit(int n) { return _abs(n % 10); }
