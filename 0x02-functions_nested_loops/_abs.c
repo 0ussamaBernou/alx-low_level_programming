c5ac943fa494a1e02ca83ae069cb1ea78c89a944
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-20 13:37:48 +0000

    .

---
+++ 0x02-functions_nested_loops/_abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute value of an int
+ *
+ * @n: number to absolute
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
