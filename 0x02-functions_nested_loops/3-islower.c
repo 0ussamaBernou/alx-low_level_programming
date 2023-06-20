bf924d5583e900941b8d86d22493e38b50c26011
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-20 12:31:02 +0000

    .

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * islower - checks if char lowercase
+ *
+ * Return: OnSucces 1. OnFail 0
+ */
+
+int islower(char c)
+{
+	int a, z;
+
+	a = 97;
+	z = a + 26;
+
+	if (c >= a && c <= z)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
