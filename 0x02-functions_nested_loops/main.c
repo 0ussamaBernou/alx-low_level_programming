d3aa51a5fc4832b84f113127e3edf42f17b6089c
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-20 13:40:38 +0000

    .

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,5 @@
 #include <unistd.h>
 
-#include "_abs.c"
-
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -10,3 +8,25 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c) { return (write(1, &c, 1)); }
+#include "main.h"
+
+/**
+ * _abs - return the absolute value of an int
+ *
+ * @n: number to absolute
+ *
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
