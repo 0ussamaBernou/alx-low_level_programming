4ddd43ddc906512564676aadbd5f15c1ef11a3e0
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-21 20:08:22 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print_to_98 function
+ *
+ * @n: start point
+ *
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i <= 98; i++)
+	{
+		_putchar('0' + (int)(i / 10));
+		_putchar('0' + (int)(i % 10));
+		if (i < 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
+
+	return (0);
+}
