ccbf72a4e93758c9661ba295aa82d4f7659675ce
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-23 23:17:25 +0000

    .

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -12,12 +12,15 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j < i + 1; j++)
-			_putchar(' ');
+		if (i != 0)
+		{
+			for (j = 0; j < i + 1; j++)
+				_putchar(' ');
 
-		_putchar('\\');
+			_putchar('\\');
+		}
 		_putchar('\n');
 	}
 }
