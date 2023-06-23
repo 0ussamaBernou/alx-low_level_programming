95f9734236ad9dabb75d010c7923e67a9bfe7e12
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-23 23:24:08 +0000

    .

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,12 +10,16 @@
 
 void print_square(int size)
 {
+	if (size <= 0)
+		_putchar('\n');
+
 	int row, col;
 
 	for (row = 0; row < size; row++)
 	{
 		for (col = 0; col < size; col++)
 			_putchar('#');
+
 		_putchar('\n');
 	}
 }
