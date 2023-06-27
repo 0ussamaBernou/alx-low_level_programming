b5735da2313e8129300de8221f2c57fe613f7022
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-27 01:02:21 +0000

    .

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,18 +10,18 @@
 
 void rev_string(char *s)
 {
-        int i = 0;
-        int j = 0;
-        char temp;
+	int i = 0;
+	int j = 0;
+	char temp;
 
-        while (*(s + i))
-                i++;
+	while (*(s + i))
+		i++;
 
-        while (j < i--)
-        {
-                temp = *(s + j);
-                *(s + j) = *(s + i);
-                *(s + i) = temp;
-                j++;
-        }
+	while (j < i--)
+	{
+		temp = *(s + j);
+		*(s + j) = *(s + i);
+		*(s + i) = temp;
+		j++;
+	}
 }
