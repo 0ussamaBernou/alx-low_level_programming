09043ca1063473f1829afd1c192cc1febcbcb6b8
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-29 19:09:59 +0000

    .

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+char *leet(char *str)
+{
+	int i;
+	char *new_str = str;
+
+	for (i = 0; new_str[i] != '\0'; i++)
+	{
+		if (new_str[i] == 'a' || new_str[i] == 'A')
+			new_str[i] = '4';
+
+		if (new_str[i] == 'e' || new_str[i] == 'E')
+			new_str[i] = '3';
+
+		if (new_str[i] == 'o' || new_str[i] == 'O')
+			new_str[i] = '0';
+
+		if (new_str[i] == 't' || new_str[i] == 'T')
+			new_str[i] = '7';
+
+		if (new_str[i] == 'l' || new_str[i] == 'L')
+			new_str[i] = '1';
+	}
+
+	return (new_str);
+}
