c603d74615889c8065c9bde88eef75acde2bcd96
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-29 14:35:58 +0000

    .

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -19,5 +19,5 @@ char *_strcat(char *dest, char *src)
 
 	dest[length + i] = '\0';
 
-	return dest;
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -20,5 +20,5 @@ char *_strcat(char *dest, char *src, int n)
 
 	dest[length + i] = '\0';
 
-	return dest;
+	return (dest);
 }
