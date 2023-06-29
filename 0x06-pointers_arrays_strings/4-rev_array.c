d3457784f05df8439726ce0ac889230878a6ea65
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-06-29 15:42:52 +0000

    .

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,3 +1,10 @@
+/**
+ * reverse_array - reverses the content of an array of integers
+ * @a: array of integers
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
 
 void reverse_array(int *a, int n)
 {
