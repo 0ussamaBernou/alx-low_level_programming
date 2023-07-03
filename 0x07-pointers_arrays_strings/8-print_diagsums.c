393ffb8403c9eb41c065ef599b97cc00b14fad49
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-03 18:56:29 +0000

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,7 +8,8 @@ void print_diagsums(int *a, int size)
 {
         int i, j, sum1 = 0, sum2 = 0;
         int **ptr;
-        ptr = (int **)a;
+
+        ptr = (int *)a;
 
         for (i = 0; i < size; i++)
         {
