f3318220374f6ddd81a5896401dec27f62e60809
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-03 18:27:12 +0000

    update

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,24 +10,21 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-        unsigned int i, j;
+	unsigned int i, j;
 
-        char *p;
+	char *p;
 
-        for ( i = 0; s[i] != '\0'; i++)
-        {
-                for ( j = 0; accept[j] != '\0'; j++)
-                {
-                        if (s[i] == accept[j])
-                        {
-                                p = &s[i];
-                                return (p) ;
-                        }
-                               
-                }
-                                
-        }
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				p = &s[i];
+				return (p);
+			}
+		}
+	}
 
-        return (NULL);
-        
+	return (NULL);
 }
