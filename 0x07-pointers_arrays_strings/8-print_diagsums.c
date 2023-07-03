63f08a7014d9eb4b47261244dc2f6e64514c1606
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-03 19:12:46 +0000

    update

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -8,12 +8,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1, sum2;
+	int i, j, sum1 = 0, sum2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += *(a + (i * size + i) * sizeof(*a));
-		sum2 += *(a + (i * size + (size - i)) * sizeof(*a));
+		sum1 += *(a + (i * size + i) * sizeof(int));
+		sum2 += *(a + (i * size + (size - i - 1)) * sizeof(int));
 	}
 
 	printf("%d, %d\n", sum1, sum2);
