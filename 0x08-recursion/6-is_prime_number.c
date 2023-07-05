d4e9dd9c7bc574ddf990fe1d912207fce383d0a2
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-05 23:15:37 +0000

    update

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -9,7 +9,7 @@
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 	return is_prime_helper(n, 2);
 }
 
@@ -23,8 +23,8 @@ int is_prime_number(int n)
 int is_prime_helper(int n, int i)
 {
 	if (i == n)
-		return 1;
+		return (1);
 	if (n % i == 0)
-		return 0;
+		return (0);
 	return is_prime_helper(n, i + 1);
 }
