47b51f21178ac4ffc5a92f7b18d404ad336422ef
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-05 23:13:38 +0000

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,12 +6,3 @@
  *
  * Return: square root of n, -1 if n does not have a natural square root
  */
-int _sqrt_recursion(int n)
-{
-	int i = 1;
-
-	if (n < 0)
-		return (-1);
-
-	return (find_sqrt(n, i));
-}
