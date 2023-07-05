bfc895628f279b00248c6817643fca82a8b5168a
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-05 23:16:59 +0000

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,3 +6,27 @@
  *
  * Return: square root of n, -1 if n does not have a natural square root
  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqrt_helper(n, 1));
+}
+
+/**
+ * sqrt_helper - helper function for _sqrt_recursion
+ * @n: number to find square root of
+ * @i: current guess for square root
+ *
+ * Return: square root of n, or -1 if n does not have a natural square root
+ */
+int sqrt_helper(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (sqrt_helper(n, i + 1));
+}
