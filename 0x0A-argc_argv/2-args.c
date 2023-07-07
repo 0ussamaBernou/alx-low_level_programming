11edf10ad0e0a7ed91b87b6ae23064751a255b0d
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-07 15:45:39 +0000

    update

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -8,8 +8,12 @@
  */
 int main(int argc, char *argv[])
 {
-	argc = argc;
+	int i;
 
-	printf("%s\n", argv[0]);
-	return (1);
+	for ( i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	
+	return (0);
 }
