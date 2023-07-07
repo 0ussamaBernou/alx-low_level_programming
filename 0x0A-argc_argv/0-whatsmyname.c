0452907b9bfa5fdc76c71fa5591f71568bc34e7a
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-07 14:05:10 +0000

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name, followed by a new line.
+ * @argc: number of arguments
+ * @argv: array of pointers to the strings (arguments)
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
