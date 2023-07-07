46bbfa1b9d71f104fa5a0c0f08dddbc80f2bb7b1
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-07 16:02:30 +0000

    update

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - prints the result of the multiplication, followed by a new line.
@@ -8,6 +9,11 @@
  */
 int main(int argc, char *argv[])
 {
+        if (argc != 3)
+        {
+                printf("Error\n");
+                return (1);
+        }
         printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 
 	return (0);
