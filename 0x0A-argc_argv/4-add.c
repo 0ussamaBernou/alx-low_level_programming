4e5abf81a39abf475c4fa833fc4ef2ffb28f3c71
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-08 18:46:18 +0000

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,6 +10,7 @@
 int main(int argc, char *argv[])
 {
 	int i, j, sum = 0;
+	char *arg;
 
 	if (argc < 2)
 	{
@@ -19,9 +20,10 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; *argv[j] != '\0'; j++)
+		arg = argv[i];
+		for (j = 0; arg[j] != '\0'; j++)
 		{
-			if (*argv[j] < '0' || *argv[j] > '9')
+			if (arg[j] < '0' || arg[j] > '9')
 			{
 				printf("Error\n");
 				return (1);
