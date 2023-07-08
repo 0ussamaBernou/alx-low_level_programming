0668fe7ad7d9af987debe3325d7ad5c10256e38e
OussamaBernou <96293508+0ussamaBernou@users.noreply.github.com>
2023-07-08 19:23:59 +0000

    update

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+int main(int argc, char *argv[])
+{
+	int i, sum, coins = 0;
+
+	int cents[] = {25, 10, 5, 2, 1};
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	sum = atoi(argv[1]);
+
+	if (sum <= 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 0; i < 5; i++)
+	{
+
+		while (sum >= cents[i])
+		{
+			sum -= cents[i];
+			coins++;
+		}
+	}
+
+	printf("%d\n", coins);
+	return (0);
+}
