097ebc251cad04d9efc936381d6cf577c291a3b6
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-13 19:37:34 +0100

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,6 +7,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *s3;
 	size_t i, k, l = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[i] != '\0')
 		i++;
 
