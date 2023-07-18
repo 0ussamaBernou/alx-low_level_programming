5467f3a01d7e53dfc8b5f4c8adf381894e9c314c
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-07-18 11:28:36 +0100

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
-
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * struct dog - dog struct
+ * @name: his name
+ * @age: his age
+ * @owner: his owner
+ *
+ * Description: dog struct
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 } dog;
 
 #endif
