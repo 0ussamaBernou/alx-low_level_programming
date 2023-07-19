9975aecdc4ea9aba46dc5f33fe8b410fb68aa81a
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-20 00:16:24 +0100

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
 void _putchar(char ch);
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
