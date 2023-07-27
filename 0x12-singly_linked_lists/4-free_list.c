7d66700ea384c5c9f3941d22e499a76e59b0834c
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-27 23:14:14 +0100

    update

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,10 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_list - frees a linked list
+ *
+ * @head: list's head
+ *
+ * Return: void
+ */
 void free_list(list_t *head)
 {
 	list_t *ptr;
-	if (!head) {
+	if (!head)
+	{
 		return;
 	}
 
