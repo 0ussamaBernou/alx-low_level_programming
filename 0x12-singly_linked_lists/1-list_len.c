defd2b1f22a5ba45cd60e06e24de1745d8ce0568
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-27 18:50:51 +0100

    update

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the length of the list
+ *
+ * @h: the pointer to the head of the list
+ *
+ * Return: length of the list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t length;
+	list_t *ptr;
+
+	ptr = (list_t *)h;
+	length = 1;
+
+	while (ptr->next != NULL)
+	{
+		length++;
+		ptr = ptr->next;
+	}
+	return (length);
+}
+
