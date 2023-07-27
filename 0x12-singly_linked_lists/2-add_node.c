2bf35793ca99c99e3c82c43a29a5a54975d85b42
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-27 19:18:49 +0100

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,15 +13,13 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	list_t **temp;
 
 	new = malloc(sizeof(list_t));
 	new->str = strdup(str);
 	new->len = strlen(str);
 
-	temp = &new;
-	new->next = (*head);
-	head = temp;
+	new->next = *head;
+	head = &new;
 
 	return (new);
 }
