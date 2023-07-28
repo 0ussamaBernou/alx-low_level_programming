93fc3ed9cd290de53cc9b98785200e268c8433bf
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-28 22:16:02 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,7 +8,7 @@
  */
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my "
-	       "back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my \
+		back!\n");
 }
 
