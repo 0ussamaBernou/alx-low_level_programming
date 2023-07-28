a6b1e555141551407451f60fe510aa32a143460e
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-28 22:14:50 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+ * print - prints before main function
+ *
+ * Return: void
+ */
 void print(void)
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my "
