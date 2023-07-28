962e2e928cbd661cbccc178062be920c40db2562
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-07-28 22:05:55 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -18,11 +18,18 @@ list_t *add_node_end(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 
 	if (!new)
+	{
+		free(new);
 		return (NULL);
+	}
 
 	new->str = strdup(str);
-	if(!new->str)
+	if (!new->str)
+	{
+		free(new->str);
+		free(new);
 		return (NULL);
+	}
 	new->len = strlen(new->str);
 	new->next = NULL;
 
