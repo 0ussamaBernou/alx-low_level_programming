eb40f8b7cddea288f75a675cb528ddb766a596d0
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-01 23:31:48 +0100

    update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -35,10 +35,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		while ((ptr)->next)
 		{
 			ptr = ((ptr)->next);
-			(ptr)->next = new;
 		}
+		(ptr)->next = new;
 	}
 
 	return (new);
 }
-
