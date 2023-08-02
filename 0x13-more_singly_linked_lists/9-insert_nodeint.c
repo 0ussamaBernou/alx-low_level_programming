8a0246954f9b1bb71c976e8b48bc3d70e51e2ef4
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-02 02:50:49 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head)
 		return (NULL);
 
-
 	new = malloc(sizeof(listint_t));
 	if (!new)
 		return (NULL);
@@ -53,3 +52,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	return (new);
 }
+
