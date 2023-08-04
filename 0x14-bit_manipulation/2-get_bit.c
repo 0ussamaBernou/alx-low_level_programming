ded47264155f6d4a6a31a6c2682ef0ce81f4f804
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-05 00:29:39 +0100

    update

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -5,7 +5,7 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned long int value;
 
 	value = (n >> index) & 1UL;
-	if (!value)
+	if (value != 0 || value != 1)
 		return (-1);
 
 	return ((int)value);
