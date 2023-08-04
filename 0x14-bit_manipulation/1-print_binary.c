7eaaf7d72e466dc4b5b0d31da7a73e3e1528e3c3
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-04 20:50:43 +0100

    update

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - print number in binary
+ *
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned int i, mask, counter;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 31; (int)i >= 0; i--)
+	{
+
+		mask = (1 << i);
+
+		if (n & mask)
+		{
+			_putchar('1');
+			counter++;
+		}
+		else if (counter > 0)
+			_putchar('0');
+	}
+}
+
