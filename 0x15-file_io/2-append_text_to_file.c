da987649e21b1505bb06ed93998ea0e9bb1fe04c
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-08 02:43:19 +0100

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,9 +25,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		if (errno == ENOENT)
 		{
-			return -1;
+			return (-1);
 		}
-		return -1;
+		return (-1);
 	}
 
 	if (text_content)
