bceebc1d88bcd162c6f05558e4864087f69289a2
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-08 02:10:53 +0100

    update

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * create_file - create a file
+ * @filename: the filename
+ * @text_content: text content
+ *
+ * Return: 1 on success -1 on fail
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int written;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_TRUNC | O_WRONLY | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	written = write(fd, text_content, strlen(text_content) + 1);
+
+	if (written == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
+
