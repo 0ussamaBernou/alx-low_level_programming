e297b5c23604034358a2744f62f1756842aaa63b
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-08-08 02:28:30 +0100

    update

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,6 +16,11 @@ int append_text_to_file(const char *filename, char *text_content)
 	int fd;
 	int written;
 
+	if (!filename)
+	{
+		return (-1);
+	}
+
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd == -1)
 		return (-1);
