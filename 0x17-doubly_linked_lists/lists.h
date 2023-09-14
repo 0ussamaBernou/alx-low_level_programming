4367f6087f5300d853c4b0f8a43264e9c7edc17b
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-14 17:48:35 +0100

    update

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -10,7 +11,6 @@
  * Description: doubly linked list node structure
  *
  */
-#include <stddef.h>
 typedef struct dlistint_s
 {
 	int n;
