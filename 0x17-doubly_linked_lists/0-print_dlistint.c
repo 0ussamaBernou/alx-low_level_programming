4ef3d7650c10d7071fec36ccbef16acf7b329f9b
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-14 17:07:43 +0100

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * print_dlistint - print dllist
+ *
+ * @h: list head
+ *
+ *
+ * Return: list size
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	dlistint_t *ptr = NULL;
