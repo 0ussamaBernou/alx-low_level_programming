2a75b4463c0476334cdc22fa4b47633a41f4264c
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-15 00:29:51 +0100

    update

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -16,6 +16,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	node = head;
 	for (i = 0; i < index; i++)
 	{
+		if (!node)
+		{
+			return (NULL);
+		}
 		node = node->next;
 	}
 
