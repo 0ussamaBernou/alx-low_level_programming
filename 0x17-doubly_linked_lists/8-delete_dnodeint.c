1b3430784334954248d2209d3dcfe140972435c8
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-15 01:16:45 +0100

    update

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <stdlib.h>
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *iterator;
+	unsigned int i;
+
+	iterator = *head;
+	if (!head || !(*head))
+	{
+		return (-1);
+	}
+	for (i = 0; i < index; i++)
+	{
+		if (!iterator)
+		{
+			return (-1);
+		}
+		iterator = iterator->next;
+	}
+
+	if (iterator->prev)
+	{
+		if (iterator->next)
+		{
+			iterator->next->prev = iterator->prev;
+			iterator->prev->next = NULL;
+		}
+		iterator->prev->next = iterator->next;
+	}
+	else
+	{
+		if (iterator->next)
+		{
+			iterator->next->prev = NULL;
+			*head = iterator->next;
+		}
+	}
+	free(iterator);
+	return (1);
+}
