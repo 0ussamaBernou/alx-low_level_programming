501d9f25673d4710391e0cd0b2671d0164415350
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-15 01:18:04 +0100

    update

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -36,6 +36,10 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			iterator->next->prev = NULL;
 			*head = iterator->next;
 		}
+		else
+		{
+			return (-1);
+		}
 	}
 	free(iterator);
 	return (1);
