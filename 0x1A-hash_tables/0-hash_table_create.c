2cc375db4420b9015e496d1007da2b9a5ad7aed2
0ussamaBernou <oussama_bernou@um5.ac.ma>
2023-10-05 13:28:51 +0100

    hash table create function

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *thead;
+
+	thead = (hash_table_t *)malloc(sizeof(hash_table_t));
+	if (!thead)
+	{
+		free(thead);
+		return (NULL);
+	}
+	thead->size = size;
+	thead->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	if (!(thead->array))
+	{
+		free(thead->array);
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+		thead->array[i] = NULL;
+
+	return (thead);
+}
