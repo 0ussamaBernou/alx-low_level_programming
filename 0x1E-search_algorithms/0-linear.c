de5447fca13b57364a897aefc472d60d830c9072
0ussamabernou <oussama.bernou@outlook.com>
2024-03-12 16:53:55 +0000

    betty fix

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,13 +13,13 @@ int linear_search(int *array, size_t size, int value)
 	int i;
 
 	if (!array || !*array)
-		return -1;
+		return (-1);
 
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%d] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return i;
+			return (i);
 	}
-	return -1;
+	return (-1);
 }
